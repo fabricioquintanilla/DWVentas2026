dfffb644e1a13f8f2c6701afff9af03cb98fa014
Fabricio Quintanilla <fabricio.quintanilla@gmail.com>
2025-06-23 21:47:14 -0600

    Create test2.sql

---
+++ test2.sql
@@ -0,0 +1,18 @@
+create database test2
+go
+
+create table cars(
+id int,
+[name] varchar(100),
+mpg float,
+cyl int,
+disp float,
+hp int,
+drat float,
+wt float,
+qsec float,
+vs int,
+am int,
+gear tinyint,
+carb tinyint)
+go
